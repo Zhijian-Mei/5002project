4d8729170745fb3900f8e7c90abd7a9b45dd0866
Zhijian Mei <545902915@qq.com>
2023-04-30 14:29:50 +0800

    Update model.py

--- model.py
+++ model.py
@@ -12,10 +12,7 @@ class MyModel(nn.Module):
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor = None):
         input_tensor = self.emb(input_tensor)
-        print(input_tensor.shape)
         encoded = self.extract(input_tensor,attention_mask)
-        print(encoded.shape)
-        quit()
         size = encoded.shape
         encoded = encoded.reshape((size[0],size[2],size[1]))
         print(encoded.shape)
