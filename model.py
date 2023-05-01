8590024d19735b9793697a6df626c219986b1a3f
Zhijian Mei <545902915@qq.com>
2023-05-01 16:41:40 +0800

    1

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 import torch.nn.functional as f
-from transformers import BertModel,BertConfig
 class MyModel(nn.Module):
 
     def __init__(self,args,input_size):
