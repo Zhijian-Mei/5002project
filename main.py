3614677aa50a7e5ca286274e1741b7182c7f2d1e
Zhijian Mei <545902915@qq.com>
2023-05-01 21:18:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
         ):
             input_, output = i[0].to(device), i[1].to(device)
             print(input_.shape)
-            print(output)
+            print(output.shape)
             quit()
             attention_mask = torch.ones((input_.shape[0], 1, ws)).to(device)
             predict = model(input_, attention_mask)
