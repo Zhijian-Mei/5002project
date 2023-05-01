530de931976ea1d8e05822376fdb06552ea2de05
Zhijian Mei <545902915@qq.com>
2023-05-01 15:52:32 +0800

    Update correlation_check.py

--- correlation_check.py
+++ correlation_check.py
@@ -8,5 +8,5 @@ for group in groups:
     id = group[0]
     data = group[1]
     print(f'Turb # {id}')
-    print(data.corr(method='pearson'))
+    print(data.corr(method='pearson').loc['Patv'] )
     print('------------------------------------------------')
