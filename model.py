367e73b1b1170c545efb5154d442565ca1fa13e8
Zhijian Mei <545902915@qq.com>
2023-05-03 13:20:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -12,16 +12,16 @@ class MyModel(nn.Module):
         self.bidirectional = True
         self.emb = nn.Linear(input_size,args.hidden_size).to(device)
         self.extract = BERT(args.hidden_size,args.hidden_size).to(device)
-        self.out = nn.Linear(args.hidden_size,1).to(device)
-        # self.project = nn.LSTM(args.hidden_size, args.hidden_size, self.lstm_layers,batch_first=True,bidirectional=self.bidirectional).to(device)
-        # self.out = nn.Linear(args.hidden_size*2 if self.bidirectional else args.hidden_size,1).to(device)
+        # self.out = nn.Linear(args.hidden_size,1).to(device)
+        self.project = nn.LSTM(args.hidden_size, args.hidden_size, self.lstm_layers,batch_first=True,bidirectional=self.bidirectional).to(device)
+        self.out = nn.Linear(args.hidden_size*2 if self.bidirectional else args.hidden_size,1).to(device)
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor = None):
         input_tensor = self.emb(input_tensor)
         encoded = self.extract(input_tensor,attention_mask)
         # h0 = torch.zeros(self.lstm_layers,encoded.shape[0],  self.args.hidden_size).to(self.device)
         # c0 = torch.zeros(self.lstm_layers,encoded.shape[0], self.args.hidden_size).to(self.device)
-        # encoded,(hn, cn) = self.project(encoded)
+        encoded,(hn, cn) = self.project(encoded)
         output = self.out(encoded).squeeze()
         return output
 
