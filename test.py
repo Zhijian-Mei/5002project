083560538a4f587ac4c031f0cb4ba78f02533795
Zhijian Mei <545902915@qq.com>
2023-05-04 16:13:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     eval_loader = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
 
     epoch = args.epoch
-    loss_fct = nn.MSELoss()
+    loss_fct = nn.MSELoss(reduction ='sum')
     best_eval_loss = np.inf
     count = 0
     for e in range(epoch):
