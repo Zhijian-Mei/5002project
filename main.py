77d47cfb76f47b9ffbff112829907f0ac3a3f023
Zhijian Mei <545902915@qq.com>
2023-05-04 20:23:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,5 +143,7 @@ if __name__ == '__main__':
                 print('saving better checkpoint')
                 print('early stop! ')
                 break
+        torch.save({'model': model.state_dict()},
+                   f'{folder_name}/best_model_{round(best_eval_loss, 3)}.pt')
         print(f'finish turbine {id}')
         print('train next turbine')
