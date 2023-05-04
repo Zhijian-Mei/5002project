3d6d1e2546fdbc5c647f7a5ae379b492f2910a67
Zhijian Mei <545902915@qq.com>
2023-05-04 22:37:38 +0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-
 def score(predict, label):
     ## inputs are two numpy array
+    print(predict.shape)
+    print(label.shape)
+    print(label - predict)
+    quit()
     left = np.sqrt(np.square(label - predict).mean())
     right = np.square(np.abs(label - predict)).mean()
     return (left + right) / 2
