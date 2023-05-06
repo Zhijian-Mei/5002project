e1148bce176a11adc0ab155972c86787aa80fff3
Zhijian Mei <545902915@qq.com>
2023-05-06 10:28:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -49,8 +49,9 @@ if __name__ == '__main__':
     in_file_root = f'data/final_phase_test/infile'
     out_file_root = f'data/final_phase_test/outfile'
     print('start testing')
-    for k in trange(1,143):
+    for k in trange(1, 143):
         score = 0
+        count = 0
         current_index = f'{k:04d}'
         in_file = f'{in_file_root}/{current_index}in.csv'
         out_file = f'{out_file_root}/{current_index}out.csv'
@@ -64,26 +65,26 @@ if __name__ == '__main__':
             id = item_in[0]
             df_in = item_in[1]
             df_out = item_out[1]
-            current_df_in = df_in[len(df_in)-288:]
+            current_df_in = df_in[len(df_in) - 288:]
             current_df_out = df_out
-            input_ = current_df_in.drop(columns=['Patv','TurbID'])
+            input_ = current_df_in.drop(columns=['Patv', 'TurbID'])
             output_ = current_df_out['Patv']
             input_ = input_.fillna(input_.mean()).values
             output_ = output_.fillna(output_.mean()).values
 
-            current_mean = mean_df[mean_df.TurbID == id][['Wspd','Wdir']].values[0]
-            current_std = std_df[std_df.TurbID == id][['Wspd','Wdir']].values[0]
+            current_mean = mean_df[mean_df.TurbID == id][['Wspd', 'Wdir']].values[0]
+            current_std = std_df[std_df.TurbID == id][['Wspd', 'Wdir']].values[0]
             current_mean = [current_mean for _ in range(288)]
             current_std = [current_std for _ in range(288)]
-            normalize_input = (input_ - current_mean)/current_std
+            normalize_input = (input_ - current_mean) / current_std
 
             patv = current_df_in[['Patv']].fillna(current_df_in[['Patv']].mean()).values
-            patv = np.where(patv < 0 , 0 , patv)
-            normalize_input = np.concatenate((normalize_input,patv),axis=1)
+            patv = np.where(patv < 0, 0, patv)
+            normalize_input = np.concatenate((normalize_input, patv), axis=1)
             # prepare checkpoint folder
             folder_name = f'checkpoint/{root_name}/turbine_{id}'
 
-            checkpoint = torch.load(f'{folder_name}/best_model.pt',map_location=device)
+            checkpoint = torch.load(f'{folder_name}/best_model.pt', map_location=device)
             model = MyModel(args, len(subset) - 1, device)
             model.load_state_dict(checkpoint['model'])
 
@@ -93,13 +94,13 @@ if __name__ == '__main__':
                 output_ = torch.from_numpy(output_)
                 attention_mask = torch.ones((1, 1, ws)).to(device)
                 predict = model(input_, attention_mask)
-                score_t = score_t_abnormal(predict.numpy(),output_.numpy())
-                print(f'turbine{id}: ',score_t)
-                score+=score_t
-
+                score_t = score_t_abnormal(predict.numpy(), output_.numpy())
+                # print(f'turbine{id}: ',score_t)
+                score += score_t
+                count += 1
+            print(score / count)
         score_per_df.append(score)
     from statistics import mean
-    final_score = round(mean(score_per_df),2)
-    print('model score: ',final_score)
 
-
+    final_score = round(mean(score_per_df), 2)
+    print('model score: ', final_score)
